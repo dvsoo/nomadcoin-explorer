e374402d3bdc5aee51988a1d9ee3057d41e5182b
dvsoo <qpzm2480@naver.com>
2018-10-17 11:50:28 +0900

    setting basic react

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { injectGlobal} from 'styled-components';
+import AppPresenter from './AppPresenter';
+import reset from 'styled-reset';
+import typography from '../../typography';
+
+const baseStyle = () => injectGlobal`
+    ${reset};
+    ${typography};
+`;
+
+class AppContainer extends Component {
+	render() {
+		baseStyle();
+		return <AppPresenter />;
+	}
+}
+
+export default AppContainer;
---
+++ src/typography.js
@@ -0,0 +1,19 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+	googleFonts: [
+		{
+			name: 'Nunito',
+			style: ['400', '700'],
+		},
+		{
+			name: 'Open Sans',
+			style: ['400'],
+		},
+	],
+	headerFontFamily: ['Nunito', 'Helvetica', 'sans-serif'],
+	bodyFontFamily: ['Open Sans', 'Helvetica', 'sans-serif'],
+	includeNormalize: false,
+});
+
+export default typography.toString();
